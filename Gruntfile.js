aebd2f9aab716527052ee08458abec047cc2ae81
xmenbro <morozov.sereoja2020@gmail.com>
2025-11-28 14:16:14 +0300

    fix: fix problem with css link in dist/

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,15 @@
 module.exports = function(grunt) {
     
     grunt.initConfig({
+        // Обработка HTML перед минификацией
+        processhtml: {
+            dist: {
+                files: {
+                    'dist/html/index.html': ['src/html/index.html']
+                }
+            }
+        },
+        
         // Минификация HTML
         htmlmin: {
             dist: {
@@ -11,7 +20,7 @@ module.exports = function(grunt) {
                     minifyCSS: true
                 },
                 files: {
-                    'dist/html/index.html': ['src/html/index.html']
+                    'dist/html/index.html': 'dist/html/index.html'
                 }
             }
         },
@@ -22,6 +31,18 @@ module.exports = function(grunt) {
                 files: {
                     'dist/css/style.min.css': ['src/css/style.css']
                 }
+            }
+        },
+        
+        // Копирование CSS файлов
+        copy: {
+            css: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/css/',
+                    src: ['*.css'],
+                    dest: 'dist/css/'
+                }]
             }
         },
         
@@ -44,6 +65,7 @@ module.exports = function(grunt) {
     });
     
     // Загрузка плагинов
+    grunt.loadNpmTasks('grunt-processhtml');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
@@ -55,6 +77,6 @@ module.exports = function(grunt) {
     grunt.registerTask('minify-html', ['htmlmin']);
     grunt.registerTask('minify-css', ['cssmin']);
     grunt.registerTask('optimize-images', ['imagemin']);
-    grunt.registerTask('build', ['clean:dist', 'htmlmin', 'cssmin', 'imagemin']);
+    grunt.registerTask('build', ['clean:dist', 'cssmin', 'copy:css', 'imagemin', 'processhtml', 'htmlmin']);
     grunt.registerTask('production', ['build']);
 };
