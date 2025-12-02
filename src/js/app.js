8d44de8b4fe52ba2941cb5443fae898f8bd1f889
xmenbro <morozov.sereoja2020@gmail.com>
2025-12-03 00:30:14 +0300

    feat(app.js): add new app class and methods

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+/**
+ * Главный файл приложения
+ * @class App
+ */
+
+// Подключаем jQuery и наши модули
+import './modules/validator.js';
+import './components/forms/sign-in-form.js';
+
+// Общая инициализация приложения
+class App {
+    constructor() {
+        this.init();
+    }
+    
+    // Инициализация
+    init() {
+        console.log('Приложение инициализировано');
+        
+        // Глобальные обработчики ошибок AJAX
+        this.setupAjaxErrorHandling();
+    }
+    
+    // Обработка ошибок
+    setupAjaxErrorHandling() {
+        $(document).ajaxError(function(event, xhr) {
+            // Глобальная обработка ошибок AJAX
+            if (xhr.status === 401) {
+                console.warn('Требуется авторизация');
+            } else if (xhr.status === 403) {
+                console.warn('Доступ запрещен');
+            }
+        });
+    }
+}
+
+// Инициализируем приложение
+$(document).ready(function() {
+    window.app = new App();
+});
