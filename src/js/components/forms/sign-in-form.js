05b85518b096692a57445068e359482038661f9a
xmenbro <morozov.sereoja2020@gmail.com>
2025-12-03 00:19:01 +0300

    feat(sign-in-form.js): add init and bind events

--- src/js/components/forms/sign-in-form.js
+++ src/js/components/forms/sign-in-form.js
@@ -8,6 +8,27 @@ class SignInForm {
         this.$loginInput = $('#login');
         this.$passwordInput = $('#passwd');
         this.validator = window.Validator;
+
+        this.init();
+    }
+
+    // Инициализация
+    init() {
+        this.bindEvents();
+    }
+    
+    // Создание ивентов
+    bindEvents() {
+        // Валидация при вводе
+        this.$loginInput.on('input', () => this.validateLogin());
+        this.$passwordInput.on('input', () => this.validatePassword());
+        
+        // Валидация при отправке формы
+        this.$form.on('submit', (e) => this.handleSubmit(e));
+        
+        // Очистка ошибок при фокусе
+        this.$loginInput.on('focus', () => this.clearError(this.$loginInput));
+        this.$passwordInput.on('focus', () => this.clearError(this.$passwordInput));
     }
 }
 
